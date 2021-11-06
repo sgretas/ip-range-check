03ee4bebff46a5ba1f1270bcbdeb39fd7dd4f517
Simon Gretas <admin@simongretas.com>
2021-11-05 21:43:26 -0400

    Refactor readFileToArray method

--- src/app.js
+++ src/app.js
@@ -1,19 +1,25 @@
 const ipRangeCheck = require('ip-range-check');
 const fs = require('fs');
 
-const ipAddresses = fs.readFileSync('./files/ipAddresses.txt', 'utf8', (err, data) => err ? console.log(err) : data);
-const ipRanges = fs.readFileSync('./files/ipRanges.txt', 'utf8', (err, data) => err ? console.log(err) : data);
+const readFileToArray = (path) => {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    return data.split('\r\n');
+  } catch (err) {
+    console.log(err);
+  }
+}
 
-const ipAddressArray = ipAddresses.split('\r\n');
-const ipRangeArray = ipRanges.split('\r\n');
+const ipAddresses = readFileToArray('./files/ipAddresses.txt');
+const ipRanges = readFileToArray('./files/ipRanges.txt');
 
-const result = ipAddressArray.map(ipAddress => {
+const result = ipAddresses.map(ipAddress => {
   let checkedIp = ipAddress;
-  ipRangeArray.forEach(ipRange => {
+  ipRanges.forEach(ipRange => {
     if (ipRangeCheck(ipAddress, ipRange)) checkedIp = 'In Range';
-  })
+  });
   return checkedIp;
-}).filter(ipAddress => ipAddress !== 'In Range').toString()
+}).filter(ipAddress => ipAddress !== 'In Range').toString();
 
 fs.writeFileSync('./files/ipOutOfBounds.txt', result);
 console.log(result);
